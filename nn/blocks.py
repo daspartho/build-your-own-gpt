bdcb60110fc4fc24c5a632fa9c073559736d5e30
daspartho <parthodas6176@gmail.com>
2023-04-11 21:21:33 +0530

    run black

--- nn/blocks.py
+++ nn/blocks.py
@@ -3,13 +3,13 @@ from torch import nn
 import torch.nn.functional as F
 
 act_map = {
-    'relu': nn.ReLU(),
-    'gelu': nn.GELU(),
-    'tanh': nn.Tanh(),
+    "relu": nn.ReLU(),
+    "gelu": nn.GELU(),
+    "tanh": nn.Tanh(),
 }
 
-class MLP(nn.Module):
 
+class MLP(nn.Module):
     def __init__(self, n_embd, n_proj, act):
         super().__init__()
 
@@ -22,8 +22,8 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-class Attention(nn.Module):
 
+class Attention(nn.Module):
     def __init__(self, n_embd, block_size, n_head, bias):
         super().__init__()
 
@@ -32,35 +32,53 @@ class Attention(nn.Module):
         self.c_attn = nn.Linear(n_embd, 3 * n_embd, bias=bias)
         # output projection
         self.c_proj = nn.Linear(n_embd, n_embd, bias=bias)
-        
+
         # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size))
-                                     .view(1, 1, block_size, block_size))
+        self.register_buffer(
+            "tril",
+            torch.tril(torch.ones(block_size, block_size)).view(
+                1, 1, block_size, block_size
+            ),
+        )
         self.n_head = n_head
         self.n_embd = n_embd
 
     def forward(self, x):
-        B, T, C = x.shape # batch size, sequence length, embedding dimensionality (n_embd)
+        (
+            B,
+            T,
+            C,
+        ) = x.shape  # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * k.shape[-1]**-0.5
-        att = att.masked_fill(self.tril[:,:,:T,:T] == 0, float('-inf'))
+        att = (q @ k.transpose(-2, -1)) * k.shape[-1] ** -0.5
+        att = att.masked_fill(self.tril[:, :, :T, :T] == 0, float("-inf"))
         att = F.softmax(att, dim=-1)
-        out = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        out = out.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        out = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        out = (
+            out.transpose(1, 2).contiguous().view(B, T, C)
+        )  # re-assemble all head outputs side by side
 
         # output projection
         return self.c_proj(out)
 
-class Model(nn.Module):
 
-    def __init__(self, vocab_size, block_size, n_embd, n_head, bias, n_proj, act): # TODO: take a object containing the custom architecture structure and parameters
+class Model(nn.Module):
+    def __init__(
+        self, vocab_size, block_size, n_embd, n_head, bias, n_proj, act
+    ):  # TODO: take a object containing the custom architecture structure and parameters
         super().__init__()
 
         self.token_embedding = nn.Embedding(vocab_size, n_embd)
@@ -69,8 +87,8 @@ class Model(nn.Module):
         self.architecture = nn.Sequential(
             Attention(n_embd, block_size, n_head, bias),
             MLP(n_embd, n_proj, act),
-            ) # TODO: implement code to build the custom architecture
-    
+        )  # TODO: implement code to build the custom architecture
+
     def forward(self, x):
         x = self.token_embedding(x) + self.position_embedding(torch.arange(x.shape[-1]))
         x = self.architecture(x)
@@ -78,18 +96,21 @@ class Model(nn.Module):
 
 
 if __name__ == "__main__":
-
-    with open('nn/input.txt', 'r', encoding='utf-8') as f:
+    with open("nn/input.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
     # here are all the unique characters that occur in this text
     chars = sorted(list(set(text)))
     vocab_size = len(chars)
     # create a mapping from characters to integers
-    stoi = { ch:i for i,ch in enumerate(chars) }
-    itos = { i:ch for i,ch in enumerate(chars) }
-    encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-    decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+    stoi = {ch: i for i, ch in enumerate(chars)}
+    itos = {i: ch for i, ch in enumerate(chars)}
+    encode = lambda s: [
+        stoi[c] for c in s
+    ]  # encoder: take a string, output a list of integers
+    decode = lambda l: "".join(
+        [itos[i] for i in l]
+    )  # decoder: take a list of integers, output a string
 
     data = torch.tensor(encode(text), dtype=torch.long)
 
@@ -102,7 +123,7 @@ if __name__ == "__main__":
     n_head = 4
     bias = False
     n_proj = 32
-    act = 'gelu'
+    act = "gelu"
     model = Model(vocab_size, block_size, n_embd, n_head, bias, n_proj, act)
     model_output = model(tokenized_input)
     print("Model Output: \n", model_output)
